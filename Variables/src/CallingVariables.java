854681cd036122fdb078b40b916cdaf066695e42
Kunal Gautam <kunal@ikunal.in>
2014-10-27 12:13:58 +0530

    Learning Variables #Step1

---
+++ Variables/src/CallingVariables.java
@@ -0,0 +1,16 @@
+
+public class CallingVariables {
+	
+	
+	public static void main(String[] args) {
+        double d = 4.5;
+        double e;
+        e = 1.4;
+        String f = new String("Wassup?");
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+	
+}
